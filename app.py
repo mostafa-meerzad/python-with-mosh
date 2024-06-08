84800ee0bc1342508b76c22ac0910efa5500f2ad
mostafa <mostafameerzad@gmail.com>
2024-06-08 21:57:07 +0430

    intro to python and ide configurations

---
+++ app.py
@@ -0,0 +1,18 @@
+import math
+
+
+message = "Hello"
+
+
+print(message)
+print(bool([]))
+print(bool({"key": "value"}))
+print(bool(''))
+print(bool(None))
+
+print(math.ceil(1.25))
+print("Hello Python")
+
+print("hi" * 5)
+
+# print "hello"
